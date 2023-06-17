18ed505543ff16d851e504173a4a1bc27b201f78
Lucas Cristovam <lucas.henriques@totvs.com.br>
2023-06-17 18:59:24 -0300

    feat: sqlAlchemy cnpj type

--- doc_br/sqlalchemy_types/__init__.py
+++ doc_br/sqlalchemy_types/__init__.py
@@ -1,1 +1,2 @@
+from .cnpj_type import CNPJType  # noqa: F401
 from .cpf_type import CPFType  # noqa: F401
---
+++ doc_br/sqlalchemy_types/cnpj_type.py
@@ -0,0 +1,41 @@
+from typing import Optional
+
+from sqlalchemy.engine.interfaces import Dialect
+from sqlalchemy.types import String, TypeDecorator
+
+from doc_br.types import CNPJ
+
+
+class CNPJType(TypeDecorator):
+    """
+    Custom SQLAlchemy type for storing CNPJ documents.
+
+    This type decorator is used to convert CNPJ objects to their plain string representation
+    when storing them in the
+    database, and to convert the plain string representation back to CNPJ objects when
+    retrieving them from the database.
+    """
+
+    impl = String
+
+    def process_bind_param(self, value: Optional[CNPJ], dialect: Dialect) -> Optional[str]:
+        """
+        Convert a CNPJ object to its plain string representation for storage.
+
+        :param value: The CNPJ object to be converted.
+        :param dialect: The SQLAlchemy dialect in use.
+        :return: The plain string representation of the CNPJ object.
+        """
+        if value is not None:
+            return value.plain
+
+    def process_result_value(self, value: Optional[str], dialect: Dialect) -> Optional[CNPJ]:
+        """
+        Convert a plain string representing a CNPJ to a CNPJ object when retrieving from the db.
+
+        :param value: The plain string representation of the CNPJ.
+        :param dialect: The SQLAlchemy dialect in use.
+        :return: The CNPJ object.
+        """
+        if value is not None:
+            return CNPJ(value)
